44a79c9c651b53363e5228d70ce551966e320dfe
wbham <wbham97@naver.com>
2023-12-29 00:12:31 +0900

    [Feat] - Thread 구분
    - 비동기적으로 처리하는 함수에서 UI를 건들일 때에는 runOnUiThread 이용

--- app/src/main/java/org/shop/notice/MainActivity.kt
+++ app/src/main/java/org/shop/notice/MainActivity.kt
@@ -2,7 +2,11 @@ package org.shop.notice
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
@@ -13,32 +17,55 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val client = OkHttpClient()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
 
-        val client = OkHttpClient()
+        var serverHost = ""
 
-        val request: Request = Request.Builder()
-            .url("http://192.168.219.102:8080")
-            .build()
-
-        val callback = object : Callback {
-            // 요청 자체가 실패하거나 통신과정에서 오류가 났을 때
-            override fun onFailure(call: Call, e: IOException) {
-                Log.e("Client", e.toString())
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
-                    Log.e("Client", "${response.body?.string()}")
-                }
-            }
+        binding.serverHostEditText.addTextChangedListener {
+            serverHost = it.toString()
         }
 
-        client.newCall(request).enqueue(callback)
+        binding.confirmButton.setOnClickListener {
+            val request: Request = Request.Builder()
+                .url("http://$serverHost:8080")
+                .build()
+
+            val callback = object : Callback {
+                // 요청 자체가 실패하거나 통신과정에서 오류가 났을 때
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.e("Client", e.toString())
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "수신에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if (response.isSuccessful) {
+                        val res = response.body?.string()
+
+                        runOnUiThread {
+                            binding.informationTextView.visibility = View.VISIBLE
+                            binding.informationTextView.text = res
+
+                            binding.serverHostEditText.isVisible = false
+                            binding.confirmButton.isVisible = false
+                        }
+                    } else {
+                        runOnUiThread {
+                            Toast.makeText(this@MainActivity, "수신에 실패했습니다.", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+                }
+            }
+
+            client.newCall(request).enqueue(callback)
+        }
 
 //        Thread {
 //            try {
