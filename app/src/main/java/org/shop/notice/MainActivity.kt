e7851b83db14d40ea3fbb642e838eaddddcb9066
wbham <wbham97@naver.com>
2023-12-28 20:17:22 +0900

    initialsetting

---
+++ app/src/main/java/org/shop/notice/MainActivity.kt
@@ -0,0 +1,15 @@
+package org.shop.notice
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.shop.notice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+    }
+}
