f707856224b4d070f19fe6bc98c16733dd49e80f
wbham <wbham97@naver.com>
2023-12-28 23:15:06 +0900

    Update MainActivity.kt
    - 소켓을 이용하여 서버의 데이터를 받아오기

--- app/src/main/java/org/shop/notice/MainActivity.kt
+++ app/src/main/java/org/shop/notice/MainActivity.kt
@@ -1,10 +1,13 @@
 package org.shop.notice
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import org.shop.notice.databinding.ActivityMainBinding
-import java.net.HttpURLConnection
-import java.net.URL
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,6 +16,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
-        
+
+        Thread {
+            try {
+                // 소켓 서버가 아닌 그냥 소켓 생성
+                val socket = Socket("192.168.219.102", 8080)
+                val printer = PrintWriter(socket.getOutputStream())
+                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+
+                printer.println("GET / HTTP/1.1")
+                printer.println("Host: 192.168.219.102:8080")
+                printer.println("User-Agent: android")
+                printer.println("\r\n")
+                printer.flush()
+
+                var input: String? = "-1"
+                while (input != null) {
+                    input = reader.readLine()
+                    Log.e("Client", input)
+                }
+                reader.close()
+                printer.close()
+                socket.close()
+            } catch (e: Exception) {
+                Log.e("Client", e.toString())
+            }
+        }.start()
     }
 }
